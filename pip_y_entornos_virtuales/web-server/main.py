6a4a14700fde0eac7d73729a8022fc91acf21031
angelDariaux <angeldariaux@gmail.com>
2023-07-24 23:06:39 -0600

    usando fastAPI

--- pip_y_entornos_virtuales/web-server/main.py
+++ pip_y_entornos_virtuales/web-server/main.py
@@ -1,4 +1,19 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+  return [1, 2, 3, 4]
+
+@app.get('/contact', response_class=HTMLResponse)  # response_class=HTMLResponse, hace que la respuesta sea interpretada como un HTML
+def get_list(): 
+  return '''
+  <h1>HOla soy una pagina</h1>
+  <p>Soy un parrafo</p>
+'''
 
 def run(): 
   store.get_categories()
