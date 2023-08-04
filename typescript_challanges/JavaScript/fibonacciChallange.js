a0c17f0ce7c862759997c7d7c9094a999b1b0470
angelDariaux <angeldariaux@gmail.com>
2023-08-03 22:45:22 -0600

    addind an outDir in the configuration, installed nodemon and typescript watch

--- typescript_challanges/JavaScript/fibonacciChallange.js
+++ typescript_challanges/JavaScript/fibonacciChallange.js
@@ -1,6 +1,4 @@
 // Fibonacci Challange
-
 // Write a TypeScript function that takes a number `n` as input and returns the `n`th number in the Fibonacci sequence. 
 // The Fibonacci sequence is a series of numbers in which each number is the sum of the two preceding ones. For example, the sequence starts 0, 1, 1, 2, 3, 5, 8, 13, ...
-
-console.log('testing')
+console.log('testing');
