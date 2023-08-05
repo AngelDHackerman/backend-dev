f4456eb369a12a46e58d1f5932ac909f2a5c58bd
angelDariaux <angeldariaux@gmail.com>
2023-08-05 14:48:41 -0600

    changing file names

---
+++ typescript_challanges/basicChallanges/03_longestString.ts
@@ -0,0 +1,7 @@
+// Longest String Challenge
+// Write a TypeScript function that takes an array of strings as input and returns the longest string. 
+// If there is more than one string with the maximum length, return the first one you find.
+
+// Input: An array of strings:  ["apple", "banana", "cherry", "date", "elderberry"]
+
+// Expected Output: The longest string in the array:  "elderberry"
