9e62bd4a48e20ba819ae5169ef6210f3b96b2dc9
angelDariaux <angeldariaux@gmail.com>
2023-08-07 21:27:29 -0600

    finishing the productType challange

--- typescript_challanges/02_TypeScript-CustomeTypes/02_productType.ts
+++ typescript_challanges/02_TypeScript-CustomeTypes/02_productType.ts
@@ -10,9 +10,16 @@ type Product = {
 };
 
 function mostExpensiveProduct(products: Product[]): Product {
-  // your code here
+  // The `reduce` function iterates over each product in the array, comparing their prices.
+  // It returns the product with the highest price by comparing the current product's price with the highest found so far.
+  return products.reduce((highest, product) => { 
+    if (product.price > highest.price) { 
+      return product;
+    }
+    return highest
+  })
 }
-
+1
 // Input:
 
 const products: Product[] = [
@@ -21,6 +28,13 @@ const products: Product[] = [
   { id: 3, name: "Product 3", description: "This is product 3", price: 30 },
 ];
 
-
 // Expected Output: { id: 3, name: "Product 3", description: "This is product 3", price: 30 }
 console.log(mostExpensiveProduct(products));
+
+const products2: Product[] = [
+  {id: 1, name: "Teclado", description: "Mechanical keyboard", price: 1_000},
+  {id: 2, name: "Monitor 34 inch", description: "ultrawide monitor curved", price: 25_000},
+  {id: 2, name: "Mouse X", description: "super light and precise mouse pro", price: 3_500},
+]
+
+console.log(mostExpensiveProduct(products2))
