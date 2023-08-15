df69c48b51769db1d943c4cd4b1a8354d781cab0
angelDariaux <angeldariaux@gmail.com>
2023-08-14 23:12:15 -0600

    escribiendo el codigo de la lambda en typscript

--- typescript_challanges/05_AWS_and_TypeScript_Challanges/lamdba_DynamoDB_Data_Retrieval/lambda/index.ts
+++ typescript_challanges/05_AWS_and_TypeScript_Challanges/lamdba_DynamoDB_Data_Retrieval/lambda/index.ts
@@ -3,3 +3,25 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 
 const dynamoDb = new DynamoDB.DocumentClient();
 const tableName = 'YourTableName';
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+  const id = event.body?.id;
+
+  const params = {
+    TableName: tableName,
+    Key: { id },
+  };
+
+  try {
+    const result = await dynamoDb.get(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result.Item),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    };
+  }
+};
