c22e0e92874568a12a2f87e74118e5c408bb9535
angelDariaux <angeldariaux@gmail.com>
2023-08-15 10:36:40 -0600

    new table created!!!

--- typescript_challanges/05_AWS_and_TypeScript_Challanges/my-dynamodb-project/create-table.ts
+++ typescript_challanges/05_AWS_and_TypeScript_Challanges/my-dynamodb-project/create-table.ts
@@ -1,12 +1,20 @@
+// This code creates a new table "UserEvents" using typescript and the AWS credentials
+
 import * as AWS from 'aws-sdk';
 
 // Configuration for DynamoDB client
 const dynamoDB = new AWS.DynamoDB({ region: 'us-east-1' }); 
 
-// Parameters of the NEW Table: 
+// Define parameters f the NEW Table: 
 const params = { 
-  TableName: 'AngelTable', // name of the new table
+  TableName: 'UserEvents', // name of the new table
   KeySchema: [{ AttributeName: 'id', KeyType: 'HASH' }],
   AttributeDefinitions: [{ AttributeName: 'id', AttributeType: 'S' }],
   ProvisionedThroughput: { ReadCapacityUnits: 5, WriteCapacityUnits: 5},
 };
+
+// Create the new table
+dynamoDB.createTable(params, (err, data) => { 
+  if (err) console.error(err);
+  else console.log('Table Created', data)
+})
