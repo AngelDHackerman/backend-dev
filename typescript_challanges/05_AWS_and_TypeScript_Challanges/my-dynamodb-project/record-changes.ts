bda5ead772efd7283f139b3ce5423ab520dec213
angelDariaux <angeldariaux@gmail.com>
2023-08-16 20:38:55 -0600

    cambios

--- typescript_challanges/05_AWS_and_TypeScript_Challanges/my-dynamodb-project/record-changes.ts
+++
@@ -1,9 +0,0 @@
-import { DynamoDBStreamEvent } from 'aws-lambda';
-import * as AWS from 'aws-sdk';
-
-// Initialize the EventBridge client to publish events to AWS EventBridge
-const eventBridge = new AWS.EventBridge();
-
-exports.handler = async (event: DynamoDBStreamEvent) => { 
-  
-}
